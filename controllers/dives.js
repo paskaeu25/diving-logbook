829f08527974c5a111fb2c4f20ec14360f99f9a7
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2023-12-07 12:50:56 +0200

    made dive log post work from dive.ejs view. Added method to get dives page

--- controllers/dives.js
+++ controllers/dives.js
@@ -1,11 +1,22 @@
 const Dive = require('../models/Dive');
 
 module.exports = {
+  getDives: async (req, res) => {
+    console.log(req.user);
+    try {
+      // const diveLogs = await Todo.find({userId:req.user.id})
+      res.render('dive.ejs', {
+        user: req.user,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
   createDive: async (req, res) => {
     try {
       await Dive.create({
         location: req.body.location,
-        Date: req.body.Date,
+        Date: req.body.date,
         depth: req.body.depth,
         notes: req.body.notes,
         userId: req.user.id,
--- routes/dives.js
+++ routes/dives.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const diveController = require('../controllers/dives');
 const { ensureAuth } = require('../middleware/auth');
 
-router.get('/', diveController.getDives);
+router.get('/', ensureAuth, diveController.getDives);
 router.post('/createDive', diveController.createDive);
 
 module.exports = router;
