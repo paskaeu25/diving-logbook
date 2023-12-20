c929a7b80eab0d8b39e459f6e02f899c4f5fb2a8
PavelasB <60621475+paskaeu25@users.noreply.github.com>
2023-12-20 14:32:23 +0200

    removed todo related files and code

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const flash = require('express-flash');
 const logger = require('morgan');
 const connectDB = require('./config/database');
 const mainRoutes = require('./routes/main');
-const todoRoutes = require('./routes/todos');
+// const todoRoutes = require('./routes/todos');
 const diveRoutes = require('./routes/dives');
 
 require('dotenv').config({ path: './config/.env' });
@@ -40,9 +40,11 @@ app.use(passport.session());
 app.use(flash());
 
 app.use('/', mainRoutes);
-app.use('/todos', todoRoutes);
+// app.use('/todos', todoRoutes);
 app.use('/dives', diveRoutes);
 
 app.listen(process.env.PORT, () => {
-  console.log('Server is running, you better catch it!');
+  console.log(
+    `Server is running on ${process.env.PORT} port, you better catch it!`
+  );
 });
